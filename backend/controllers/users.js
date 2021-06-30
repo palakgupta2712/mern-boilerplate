79c0bb1f203b5a7fbf675ffd8e13356fb74a6262
Palak Gupta <palakgupta2712@gmail.com>
2021-06-30 18:26:38 +0530

    backend: users controller updated to get list of users from the database

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -1,8 +1,15 @@
 import express from "express";
+import userModel from "../models/user.model.js";
+
 const router = express.Router();
 
-export function getUser(req, res) {
-  res.send("GET request.");
-}
+export const getUser = async (req, res) => {
+  try {
+    const users = await userModel.find();
+    res.status(200).json(users);
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+};
 
 export default router;
