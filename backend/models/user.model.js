325bd7d8a4d945bf1d35c15434607c99d7301c8f
Palak Gupta <palakgupta2712@gmail.com>
2021-06-30 17:00:43 +0530

    backend: User model created

---
+++ backend/models/user.model.js
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+
+const userSchema = mongoose.Schema(
+  {
+    username: { type: String, required: true, unique: true },
+    age: Number,
+  },
+  { createdAt: { type: Date, default: new Date() } }
+);
+
+export default mongoose.model("User", userSchema);
