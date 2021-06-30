1db8747fb5724c1b0c8a5cfb38cca96ff44d203d
Palak Gupta <palakgupta2712@gmail.com>
2021-06-30 18:13:25 +0530

    backend: routes added

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,8 @@ import cors from "cors";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 
+import userRoutes from "./routes/users.js";
+
 const app = express();
 dotenv.config();
 
@@ -11,16 +13,15 @@ app.use(json());
 
 const port = process.env.PORT || 5000;
 
+// Setting up routes for
+app.use("/users", userRoutes);
+
 //Connecting to the database
 const uri = process.env.ATLAS_URI;
 mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 const connection = mongoose.connection;
 connection.once("open", () => {
   console.log("MONGODB database connection established.");
-});
-
-app.get("/", (req, res) => {
-  res.send("Hello World!");
 });
 
 app.listen(port, () => {
