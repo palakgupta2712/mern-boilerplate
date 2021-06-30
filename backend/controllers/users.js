5e03d1c43f562830a9ee88479775e920b5dcaf9d
Palak Gupta <palakgupta2712@gmail.com>
2021-06-30 19:02:58 +0530

    backend: endpoint added to create new user

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -12,4 +12,18 @@ export const getUser = async (req, res) => {
   }
 };
 
+export const createUser = async (req, res) => {
+  const { username, age } = req.body;
+  const newUser = new userModel({
+    username,
+    age,
+  });
+  try {
+    await newUser.save();
+    res.status(201).json(newUser);
+  } catch (error) {
+    res.status(409).json({ message: error.message });
+  }
+};
+
 export default router;
--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,7 +1,7 @@
 import express from "express";
-import { getUser } from "../controllers/users.js";
+import { getUser, createUser } from "../controllers/users.js";
 const router = express.Router();
 
 router.get("/", getUser);
-
+router.post("/", createUser);
 export default router;
