f3f40cc48c648bce4544f076c2f2d70b3355ffd7
Palak Gupta <palakgupta2712@gmail.com>
2021-06-30 16:40:23 +0530

    backend: connection to the database

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,21 @@
 const express = require("express");
 const cors = require("cors");
+const mongoose = require("mongoose");
 const app = express();
-const port = 5000;
+require("dotenv").config();
 
 app.use(cors());
 app.use(express.json());
+
+const port = process.env.PORT || 5000;
+
+//Connecting to the database
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+const connection = mongoose.connection;
+connection.once("open", () => {
+  console.log("MONGODB database connection established.");
+});
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
